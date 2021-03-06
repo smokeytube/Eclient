85a5be998adfb788f19719175907e52688be7f91
smokeytube <67290867+smokeytube@users.noreply.github.com>
2021-03-06 00:07:06 -0500

    Hooked up json rewriting to one button
    God damn it finally works goodnight

---
+++ src/main/java/org/Eclient/client/gui/EclientGUI.py
@@ -0,0 +1,21 @@
+import eel
+import json
+import os
+
+eel.init((os.getcwd()+'\\src\\main\\java\\org\\Eclient\\client\\gui\\web\\'))
+
+
+
+@eel.expose
+def togglemodule(modulenm):
+    with open(os.getenv('AppData')+'\\.minecraft\\mcpipy\\src\\main\\java\\org\\Eclient\\client\\Eclientconfig.json', 'r') as configpath:
+        json_data = json.load(configpath)
+        isenabled = json_data['modules'][modulenm]['Enabled']
+        json_data['modules'][modulenm]['Enabled'] = not json_data['modules'][modulenm]['Enabled']
+
+    with open(os.getenv('AppData')+'\\.minecraft\\mcpipy\\src\\main\\java\\org\\Eclient\\client\\Eclientconfig.json', 'w') as configs:
+        json.dump(json_data, configs, indent=2)
+        return str(isenabled)
+
+
+eel.start('EclientGUI.html')
